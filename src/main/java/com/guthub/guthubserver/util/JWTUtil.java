82cbf4a7fd9a2526e372eeb4304d8dd31090a9e5
MoseT1 <kimmose981214@gmail.com>
2025-12-13 10:20:04 +0900

    refactor (security): extend JWT access token expiration to 24 hours

--- src/main/java/com/guthub/guthubserver/util/JWTUtil.java
+++ src/main/java/com/guthub/guthubserver/util/JWTUtil.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 public class JWTUtil {
 
     private final SecretKey secretKey;
-    private final Long accessTokenExpiresIn = 3600L * 1000;
+    private final Long accessTokenExpiresIn = 86400L * 1000;
     private final Long refreshTokenExpiresIn = 604800L * 1000;
 
     public JWTUtil(@Value("${jwt.secret.key}") String secretKeyString) {
